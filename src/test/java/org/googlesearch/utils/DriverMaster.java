246fbe8850fac6013a508f41cc530c5441756dab
Vasko <vaskocuturilo@rambler.ru>
2017-03-06 17:32:11 +0300

    Commit at 06/03/2017

--- src/test/java/org/googlesearch/utils/DriverMaster.java
+++ src/test/java/org/googlesearch/utils/DriverMaster.java
@@ -3,36 +3,57 @@ package org.googlesearch.utils;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
 public class DriverMaster {
-       private static final String HTTP_GOOGLE_COM_NCR = "http://google.com/ncr";
-       protected static final String SEARCH_TEXT = "selenium";
-       protected static final String VALIDATE_TEXT = "www.seleniumhq.org";
-       public static final String ImageName = "i48MSmX01sE18M:";
-       public static final String  FirstLink = "//*[@class='_Rm' and contains(text(),'www.seleniumhq.org')]";
-       public static final String FirstImage = "//*[@class='irc_ho' and contains(text(),'www.seleniumhq.org')]";
-       public static final String MainField = "lst-ib";
-       public static final String LinkImages = "Images";
-       public static final String LinkAll = "All";
-       protected WebDriver driver;
+    private static final String HTTP_GOOGLE_COM_NCR = "http://google.com/ncr!!!!!!!!";
+    protected static final String SEARCH_TEXT = "selenium";
+    protected static final String VALIDATE_TEXT = "www.seleniumhq.org";
+    public static final String ImageName = "i48MSmX01sE18M:";
+    public static final String  FirstLink = "//*[@class='_Rm' and contains(text(),'www.seleniumhq.org')]";
+    public static final String FirstImage = "//*[@class='irc_ho' and contains(text(),'www.seleniumhq.org')]";
+    public static final String MainField = "lst-ib";
+    public static final String LinkImages = "Images";
+    public static final String LinkAll = "All";
+    protected WebDriver driver;
+    static String browser;
 
-       @Before
-       public void SetUp() throws Exception {
-           if (driver == null) {
-               driver = new FirefoxDriver();
-               driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-               driver.manage().window().maximize();
-               driver.get(HTTP_GOOGLE_COM_NCR);
+    @Before
+    public void SetUp() throws Exception {
+        if (driver == null) {
+            SetBrowser();
+          switch (browser) {
+              case "Firefox":
+                  driver = new FirefoxDriver();
+                  break;
+              case "Chrome":
+                  driver = new ChromeDriver();
+                  break;
+              default:
+                  driver = new FirefoxDriver();
+                  break;
+          }
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+            driver.get(HTTP_GOOGLE_COM_NCR);
 
-           }
-       }
+        }
+    }
 
-       @After
-       public void tearDown() throws Exception {
-           if (driver != null) {
-               driver.quit();
-           }
-       }
-   }
+    @After
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+
+    public static void SetBrowser () {
+
+        browser = "Firefox";
+    }
+
+
+}
