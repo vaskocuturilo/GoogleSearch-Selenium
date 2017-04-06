b8089ffcc970151fb18e662378a94a4c8fe8e490
Vasko <vaskocuturilo@rambler.ru>
2017-04-06 17:31:03 +0300

    Commit at 06/04/2017

--- src/test/java/org/googlesearch/main/GoogleTest.java
+++ src/test/java/org/googlesearch/main/GoogleTest.java
@@ -5,9 +5,8 @@ import org.googlesearch.utils.ResultSearch;
 import org.googlesearch.utils.SearchGoogle;
 import org.junit.Test;
 
-
-import static junit.framework.TestCase.assertTrue;
 import static org.googlesearch.utils.TestProperties.getProperties;
+import static org.junit.Assert.assertTrue;
 
 
 public class GoogleTest extends DriverMaster {
@@ -23,8 +22,8 @@ public class GoogleTest extends DriverMaster {
         ResultSearch allImagesOnImagePage = imagesGooglePage.searchImage();
         assertTrue(allImagesOnImagePage.getFirstImage().contains(getProperties("VALIDATE_TEXT")));
 
-        allLinksOnMainPage = googlePage.search();
-        assertTrue(allLinksOnMainPage.getFirstLink().contains(getProperties("VALIDATE_TEXT")));
+        ResultSearch allLinksOnMain = googlePage.search();
+        assertTrue(allLinksOnMain.getFirstLink().contains(getProperties("VALIDATE_TEXT")));
     }
 }
 
