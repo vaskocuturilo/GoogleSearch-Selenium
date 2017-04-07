8a4ccd566d58f3640b1295a5ff0539dbac8b3eff
Vasko <vaskocuturilo@rambler.ru>
2017-04-07 10:39:57 +0300

    Commit at 07/04/2017

--- src/test/java/org/googlesearch/main/GoogleTest.java
+++ src/test/java/org/googlesearch/main/GoogleTest.java
@@ -5,7 +5,7 @@ import org.googlesearch.utils.ResultSearch;
 import org.googlesearch.utils.SearchGoogle;
 import org.junit.Test;
 
-import static org.googlesearch.utils.TestProperties.getProperties;
+import static org.googlesearch.utils.TestProperties.getProperty;
 import static org.junit.Assert.assertTrue;
 
 
@@ -14,18 +14,18 @@ public class GoogleTest extends DriverMaster {
     @Test
     public void search() throws InterruptedException {
 
-        driver.get(getProperties("GOOGLE_URL"));
+        driver.get(getProperty("GOOGLE_URL"));
 
         SearchGoogle google = new SearchGoogle(driver);
-        ResultSearch resultSearch = google.doSearch(getProperties("SEARCH_TEXT"));
-        assertTrue(resultSearch.getFirstLink().contains(getProperties("VALIDATE_TEXT")));
+        ResultSearch resultSearch = google.doSearch(getProperty("SEARCH_TEXT"));
+        assertTrue(null,resultSearch.getFirstLink().contains(getProperty("VALIDATE_TEXT")));
 
         SearchGoogle imagesGooglePage = new SearchGoogle(driver);
         ResultSearch searchImage = imagesGooglePage.doSearchImage();
-        assertTrue(searchImage.getFirstImage().contains(getProperties("VALIDATE_TEXT")));
+        assertTrue(null,searchImage.getFirstImage().contains(getProperty("VALIDATE_TEXT")));
 
         ResultSearch search = google.doSearch();
-        assertTrue(search.getFirstLink().contains(getProperties("VALIDATE_TEXT")));
+        assertTrue(null, search.getFirstLink().contains(getProperty("VALIDATE_TEXT")));
     }
 }
 
--- src/test/java/org/googlesearch/utils/DriverMaster.java
+++ src/test/java/org/googlesearch/utils/DriverMaster.java
@@ -10,11 +10,8 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import static org.googlesearch.utils.TestProperties.getProperties;
-import static org.googlesearch.utils.TestProperties.readPropertiesFromFile;
-import static org.openqa.selenium.remote.BrowserType.CHROME;
-import static org.openqa.selenium.remote.BrowserType.FIREFOX;
-import static org.openqa.selenium.remote.BrowserType.IE;
+import static org.googlesearch.utils.TestProperties.getProperty;
+import static org.openqa.selenium.remote.BrowserType.*;
 
 public class DriverMaster {
     public static final String PROP_FILE = "testdata.properties";
@@ -25,35 +22,25 @@ public class DriverMaster {
     public static final String LinkImages = "Images";
     public static final String LinkAll = "All";
     protected WebDriver driver;
-    private static final String geckoDriver = "webdriver.gecko.driver";
 
     @Before
     public void Init() throws IOException {
-            readPropertiesFromFile();
-            String classpath = DriverMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-            classpath = classpath.replaceAll("/target/classes/", "");
-            classpath = classpath.substring(1);
-            switch (getProperties("Browser")) {
+            switch (getProperty("Browser")) {
                 case FIREFOX:
-                    System.setProperty(geckoDriver, classpath + "/src/main/resources/geckodriver.exe");
                     driver = new FirefoxDriver();
                     break;
                 case CHROME:
-                    System.setProperty(geckoDriver, classpath + "/src/main/resources/chromedriver.exe");
                     driver = new ChromeDriver();
                     break;
                 case IE:
-                    System.setProperty(geckoDriver, classpath + "/src/main/resources/IEDriverServer.exe");
                     driver = new InternetExplorerDriver();
                     break;
                 default:
-                    System.setProperty(geckoDriver, classpath + "/src/main/resources/geckodriver.exe");
                     driver = new FirefoxDriver();
                     break;
             }
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.manage().window().maximize();
-            driver.get(getProperties("GOOGLE_URL"));
         }
 
     @After
