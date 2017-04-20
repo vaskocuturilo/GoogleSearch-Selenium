69c6e8042511dca8818fc567534dbd51b96dedc7
VaskoCuturilo <vaskocuturilo@rambler.ru>
2017-04-20 20:54:00 +0300

    Commit at 20/04/2017

--- src/test/java/org/googlesearch/main/GoogleTest.java
+++ src/test/java/org/googlesearch/main/GoogleTest.java
@@ -3,23 +3,28 @@ package org.googlesearch.main;
 import org.googlesearch.utils.DriverMaster;
 import org.googlesearch.utils.ResultSearch;
 import org.googlesearch.utils.SearchGoogle;
+import org.junit.Assert;
 import org.junit.Test;
 
 
 import static junit.framework.TestCase.assertTrue;
+import static org.googlesearch.utils.PropertyLoader.loadProperty;
 import static org.googlesearch.utils.TestProperties.getProperties;
 
 
 public class GoogleTest extends DriverMaster {
 
     @Test
-    public void GoogleSearch() throws Exception {
+    public void GoogleSearch() throws InterruptedException {
+        driver.get(getProperties("GOOGLE_URL"));
+
 
         SearchGoogle googlePage = new SearchGoogle(driver);
-        ResultSearch allLinksOnMainPage = googlePage.search(getProperties("SEARCH_TEXT"));
+        SearchGoogle imagesGooglePage = new SearchGoogle(driver);
+
+        ResultSearch allLinksOnMainPage = googlePage.search(loadProperty("SEARCH_TEXT"));
         assertTrue(allLinksOnMainPage.getFirstLink().contains(getProperties("VALIDATE_TEXT")));
 
-        SearchGoogle imagesGooglePage = new SearchGoogle(driver);
         ResultSearch allImagesOnImagePage = imagesGooglePage.searchImage();
         assertTrue(allImagesOnImagePage.getFirstImage().contains(getProperties("VALIDATE_TEXT")));
 
--- src/test/java/org/googlesearch/utils/DriverMaster.java
+++ src/test/java/org/googlesearch/utils/DriverMaster.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 import static org.googlesearch.utils.TestProperties.getProperties;
 import static org.googlesearch.utils.TestProperties.readPropertiesFromFile;
@@ -25,8 +27,7 @@ public class DriverMaster {
     protected WebDriver driver;
     private static final String geckoDriver = "webdriver.gecko.driver";
     @Before
-    public void SetUp() throws Exception {
-        if (driver == null) {
+    public void Init() throws IOException {
             readPropertiesFromFile();
             String classpath = DriverMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath();
             classpath = classpath.replaceAll("/target/classes/", "");
@@ -51,11 +52,10 @@ public class DriverMaster {
             }
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.manage().window().maximize();
-            driver.get(getProperties("GOOGLE_URL"));
         }
-    }
+
     @After
-    public void tearDown() throws Exception {
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
