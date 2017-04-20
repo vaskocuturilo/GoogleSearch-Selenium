b7b81f995e5d735705846033e8ad745c9a25379b
VaskoCuturilo <vaskocuturilo@rambler.ru>
2017-04-20 20:59:54 +0300

    Commit at 20/04/2017

--- src/test/java/org/googlesearch/main/GoogleTest.java
+++ src/test/java/org/googlesearch/main/GoogleTest.java
@@ -3,33 +3,29 @@ package org.googlesearch.main;
 import org.googlesearch.utils.DriverMaster;
 import org.googlesearch.utils.ResultSearch;
 import org.googlesearch.utils.SearchGoogle;
-import org.junit.Assert;
 import org.junit.Test;
 
 
 import static junit.framework.TestCase.assertTrue;
 import static org.googlesearch.utils.PropertyLoader.loadProperty;
-import static org.googlesearch.utils.TestProperties.getProperties;
-
 
 public class GoogleTest extends DriverMaster {
 
     @Test
     public void GoogleSearch() throws InterruptedException {
-        driver.get(getProperties("GOOGLE_URL"));
-
+        driver.get(loadProperty("GOOGLE_URL"));
 
         SearchGoogle googlePage = new SearchGoogle(driver);
         SearchGoogle imagesGooglePage = new SearchGoogle(driver);
 
         ResultSearch allLinksOnMainPage = googlePage.search(loadProperty("SEARCH_TEXT"));
-        assertTrue(allLinksOnMainPage.getFirstLink().contains(getProperties("VALIDATE_TEXT")));
+        assertTrue(allLinksOnMainPage.getFirstLink().contains(loadProperty("VALIDATE_TEXT")));
 
         ResultSearch allImagesOnImagePage = imagesGooglePage.searchImage();
-        assertTrue(allImagesOnImagePage.getFirstImage().contains(getProperties("VALIDATE_TEXT")));
+        assertTrue(allImagesOnImagePage.getFirstImage().contains(loadProperty("VALIDATE_TEXT")));
 
         allLinksOnMainPage = googlePage.search();
-        assertTrue(allLinksOnMainPage.getFirstLink().contains(getProperties("VALIDATE_TEXT")));
+        assertTrue(allLinksOnMainPage.getFirstLink().contains(loadProperty("VALIDATE_TEXT")));
     }
 }
 
