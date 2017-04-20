d738afae1b6731a4a646e87192e63dd4caccbde7
VaskoCuturilo <vaskocuturilo@rambler.ru>
2017-04-20 21:16:08 +0300

    Commit at 20/04/2017

--- src/test/java/org/googlesearch/utils/DriverMaster.java
+++ src/test/java/org/googlesearch/utils/DriverMaster.java
@@ -9,15 +9,15 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import static org.googlesearch.utils.TestProperties.getProperties;
-import static org.googlesearch.utils.TestProperties.readPropertiesFromFile;
+
+import static org.googlesearch.utils.PropertyLoader.loadProperty;
 import static org.openqa.selenium.remote.BrowserType.CHROME;
 import static org.openqa.selenium.remote.BrowserType.FIREFOX;
 import static org.openqa.selenium.remote.BrowserType.IE;
 
 
 public class DriverMaster {
-    public static final String PROP_FILE = "testdata.properties";
+    public static final String PROP_FILE = "/testdata.properties";
     public static final String ImageName = "i48MSmX01sE18M:";
     public static final String  FirstLink = "//*[@class='_Rm' and contains(text(),'www.seleniumhq.org')]";
     public static final String FirstImage = "//*[@class='irc_ho' and contains(text(),'www.seleniumhq.org')]";
@@ -27,12 +27,11 @@ public class DriverMaster {
     protected WebDriver driver;
     private static final String geckoDriver = "webdriver.gecko.driver";
     @Before
-    public void Init() throws IOException {
-            readPropertiesFromFile();
+    public void Init() {
             String classpath = DriverMaster.class.getProtectionDomain().getCodeSource().getLocation().getPath();
             classpath = classpath.replaceAll("/target/classes/", "");
             classpath = classpath.substring(1);
-            switch (getProperties("Browser")) {
+            switch (loadProperty("Browser")) {
                 case FIREFOX:
                    System.setProperty(geckoDriver, classpath + "/src/main/resources/geckodriver.exe");
                     driver = new FirefoxDriver();
